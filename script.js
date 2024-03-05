27f4f954666065b412f0cd652d9a803d8b53bf8a
Cristian Se <114921009+NXTCHRIS@users.noreply.github.com>
2024-03-05 16:50:06 +0200

    final touch plus error handling

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 
 addEventListener("DOMContentLoaded", function(){
 
-
+    // setting up API Key and targeting DOM elements
     const apiKey = "ae55ac2c89cff7621cb848a61b1e6e66";
     const mainBtn = document.querySelector(".main-btn");
     const inputBox = document.getElementById("input-box")
-
-
+    const error = document.querySelector(".error-text")
+    // making the API call as per OPENWHEATERAPI DOC
     async function checkWeather(city) {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`);
         var data = await response.json()
-    
-        console.log(data)
+        // HANDLING ERRORS
+        if(data.message === 'city not found') {
+            error.style.display = "block"
+        } else {
+            error.style.display= "none"
+        }
+        // DISPLAY WHEATHER DATA IF API CALL SUCCESFUL
         document.querySelector(".city-name").innerHTML = data.name;
         document.querySelector(".temperature").innerHTML= Math.round(data.main.temp) + "°C"
         document.querySelector(".percentages").innerHTML= data.main.humidity + "%"
         document.querySelector(".kmperhour").innerHTML=data.wind.speed
     }
     
+    // listening when the user clicks on search btn 
     mainBtn.addEventListener('click', () => {
         checkWeather(inputBox.value);
     })
-
-
 })
 
 
